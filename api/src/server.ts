94f52abc45c0db3960251b96b6681135c8555414
jaac25 <acevedoalexander52@gmail.com>
2026-03-13 12:46:40 -0500

    Swagger implementation

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,6 +12,29 @@ import { seedCharacters } from "./seeders/character.seed";
 import { sequelize } from "./config/database";
 import { startCharacterCron } from "./cron/character.cron";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Rick & Morty GraphQL API",
+      version: "1.0.0",
+      description:
+        "API built with Express, GraphQL, Sequelize and Redis cache.",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: ["./src/server.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
 const startServer = async () => {
   const app = express();
 
@@ -44,6 +67,76 @@ const startServer = async () => {
     next();
   });
 
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+  /**
+   * @swagger
+   * /graphql:
+   *   post:
+   *     summary: GraphQL endpoint
+   *     description: Execute GraphQL queries and mutations.
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               query:
+   *                 type: string
+   *           examples:
+   *             getCharacters:
+   *               summary: Get characters
+   *               value:
+   *                 query: |
+   *                   query {
+   *                     characters(name: "Rick") {
+   *                       id
+   *                       name
+   *                       species
+   *                       image
+   *                     }
+   *                   }
+   *             getComments:
+   *               summary: Get comments of a character
+   *               value:
+   *                 query: |
+   *                   query {
+   *                     comments(characterId: "1") {
+   *                       id
+   *                       comment
+   *                       createdAt
+   *                     }
+   *                   }
+   *             toggleFavorite:
+   *               summary: Toggle favorite character
+   *               value:
+   *                 query: |
+   *                   mutation {
+   *                     toggleFavorite(id: "1", isFavorite: true)
+   *                   }
+   *             addComment:
+   *               summary: Add comment to character
+   *               value:
+   *                 query: |
+   *                   mutation {
+   *                     addComment(characterId: "1", comment: "Great character!") {
+   *                       id
+   *                       comment
+   *                       createdAt
+   *                     }
+   *                   }
+   *             deleteCharacter:
+   *               summary: Soft delete character
+   *               value:
+   *                 query: |
+   *                   mutation {
+   *                     deleteCharacter(id: "1")
+   *                   }
+   *     responses:
+   *       200:
+   *         description: GraphQL response
+   */
   app.use(
     "/graphql",
     cors({ origin: "*" }),
